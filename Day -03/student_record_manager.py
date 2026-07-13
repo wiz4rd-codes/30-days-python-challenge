ac637322f2ff60d6d20aec9f52c26a6d0b461a0f
Coder Wizard <anmol8saini27@gmail.com>
2026-07-13 15:53:32 +0530

    Added student record manager using File Handling

--- Day -03/student_record_manager.py
+++ Day -03/student_record_manager.py
@@ -1,29 +1,38 @@
-o = int(input("Enter 1 for Storing student record\nEnter 2 for Display student record\nEnter your number : "))
+o = int(input("Enter 1 for Storing Student Record\nEnter 2 for Display Student Record\nEnter your number : "))
+
+
 def store_data():
-n = int(input("Enter total number of students : "))
-for i in range (0,n):
-name = input(f"Enter name of the student number {i+1} : ")
-roll = int(input(f"Enter roll no. of the student number {i+1} : "))
-marks = int(input(f"Enter marks of the student number {i+1} : "))
-record = f"{name},{roll},{marks}"
-with open("Student.txt", "a") as f:
-f.writelines(record + "\n")
-print("Student record saved successfully")
+    n = int(input("Enter total number of students : "))
+
+    for i in range(n):
+        name = input(f"Enter name of student {i + 1} : ")
+        roll = int(input(f"Enter roll number of student {i + 1} : "))
+        marks = int(input(f"Enter marks of student {i + 1} : "))
+
+        record = f"{name},{roll},{marks}"
+
+        with open("Student.txt", "a") as f:
+            f.writelines(record + "\n")
+
+    print("Student record saved successfully")
+
+
 def display_data():
+    with open("Student.txt", "r") as f:
+        f.seek(0)
 
-with open("Student.txt", "r") as f:  
-    f.seek(0)  
-    full_data = f.readlines()  
-    for line in full_data:  
-        data = line.strip("\n")  
-        print(data)  
-   
-    print(f"File pointer is at {f.tell()} at the end of the file ")
+        full_data = f.readlines()
 
-if(o ==1):
-store_data()
-elif(o==2):
-display_data()
+        for line in full_data:
+            data = line.strip("\n")
+            print(data)
+
+        print(f"\nFile pointer is at {f.tell()} (End of File)")
+
+
+if o == 1:
+    store_data()
+elif o == 2:
+    display_data()
 else:
-print("Invalid input")
-
+    print("Invalid input")
